728e33c3e89556af89a972a3e3540c2b099d7165
Jonathan <jonathanbry18@gmail.com>
2022-09-23 11:44:38 -0700

    1

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -1,6 +1,9 @@
 // constants
 import Web3EthContract from "web3-eth-contract";
 import Web3 from "web3";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+
 // log
 import { fetchData } from "../data/dataActions";
 
@@ -88,6 +91,99 @@ export const connect = () => {
       }
     } else {
       alert("Install Metamask.");
+      //  Create WalletConnect Provider
+      const provider = new WalletConnectProvider({
+        infuraId: "4fd3dd38c4474553b33ded0ddd28a0da", // Required
+        qrcodeModalOptions: {
+          desktopLinks: [
+            'ledger',
+            'metamask',
+            'wallet',
+            'wallet 3',
+            'secuX',
+            'ambire',
+            'wallet3',
+            'apolloX',
+            'zerion',
+            'sequence',
+            'punkWallet',
+            'kryptoGO',
+            'nft',
+            'riceWallet',
+            'vision',
+            'keyring'
+          ],
+          mobileLinks: [
+            "rainbow",
+            "metamask",
+            "argent",
+            "trust",
+            "imtoken",
+            "pillar",
+          ],
+        }
+      });
+
+
+      
+    const abiResponse = await fetch("/config/abi.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const abi = await abiResponse.json();
+    const configResponse = await fetch("/config/config.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const CONFIG = await configResponse.json();
+      //  Enable session (triggers QR Code modal)
+      await provider.enable();
+
+      //  Create Web3
+      let web3 = new Web3(provider);
+
+      const chainId = await web3.eth.getChainId();
+
+      console.log(chainId, "chain ID, WC WORKS!");
+
+      Web3EthContract.setProvider(provider);
+      if (!metamaskIsInstalled) {
+      try {
+        const accounts = await web3.eth.getAccounts();
+        const networkId = await web3.eth.net.getId();
+        if (networkId == CONFIG.NETWORK.ID) {
+          const SmartContractObj = new Web3EthContract(
+            abi,
+            CONFIG.CONTRACT_ADDRESS
+          );
+          dispatch(
+            connectSuccess({
+              account: accounts[0],
+              smartContract: SmartContractObj,
+              web3: web3,
+            })
+          );
+          // Add listeners start
+          provider.on("accountsChanged", (accounts) => {
+            dispatch(updateAccount(accounts[0]));
+          });
+          provider.on("chainChanged", () => {
+            window.location.reload();
+          });
+          // Add listeners end
+        } else {
+          dispatch(connectFailed(`Change network to ${CONFIG.NETWORK.NAME}.`));
+        }
+      } catch (err) {
+        dispatch(connectFailed("Something went wrong."));
+      }
+    }
+
+
     }
   };
 };
