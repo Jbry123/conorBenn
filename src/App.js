891b3dc66155d1f3e6447f880aa36b7110745c92
Jonathan <jonathanbry18@gmail.com>
2022-09-23 07:08:45 -0700

    1

--- src/App.js
+++ src/App.js
@@ -929,7 +929,7 @@ function App() {
     const priceResponse = await fetch("https://api.etherscan.io/api?module=stats&action=ethprice&apikey=HXZZM4ESINEDGTYQS4Z17DMG54QCV11M45", {
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json",
+        Accept: "/*/",
          
       },
     });
